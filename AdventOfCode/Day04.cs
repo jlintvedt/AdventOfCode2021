cb7485d4e7af0f0d6d0b9b3c0874d130a96f4b67
jlintvedt <jlintvedt@gmail.com>
2021-11-30 11:01:04 +0100

    Baseline

---
+++ AdventOfCode/Day04.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// https://adventofcode.com/2021/day/04
+    /// </summary>
+    public class Day04
+    {
+        // == == == == == Puzzle 1 == == == == ==
+        public static string Puzzle1(string input)
+        {
+            return "Puzzle1";
+        }
+
+        // == == == == == Puzzle 2 == == == == ==
+        public static string Puzzle2(string input)
+        {
+            return "Puzzle2";
+        }
+    }
+}
