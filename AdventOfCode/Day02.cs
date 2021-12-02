f27578b0cca99ff19a6df581e7439a392d195d01
jlintvedt <jlintvedt@gmail.com>
2021-12-02 06:14:33 +0100

    Solve Day02 Puzzle1

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode
 {
@@ -7,15 +9,61 @@ namespace AdventOfCode
     /// </summary>
     public class Day02
     {
+        public class SubmarineDive
+        {
+            private int distance = 0;
+            private int depth = 0;
+            private List<string> moves;
+
+            public SubmarineDive(string input)
+            {
+                moves = input.Split(Environment.NewLine).ToList();
+            }
+
+            public int ExecuteMoves()
+            {
+                foreach (var move in moves)
+                {
+                    ApplyMoveCommand(move);
+                }
+
+                return distance * depth;
+            }
+
+            private void ApplyMoveCommand(string command)
+            {
+                var parts = command.Split(' ');
+
+                switch (parts[0])
+                {
+                    case "forward":
+                        distance += int.Parse(parts[1]);
+                        break;
+                    case "down":
+                        depth += int.Parse(parts[1]);
+                        break;
+                    case "up":
+                        depth -= int.Parse(parts[1]);
+                        break;
+                    default:
+                        throw new ArgumentException($"Unknown move {command}");
+                }
+            }
+        } 
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var sd = new SubmarineDive(input);
+
+            return sd.ExecuteMoves().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
         public static string Puzzle2(string input)
         {
+            var sd = new SubmarineDive(input);
+
             return "Puzzle2";
         }
     }
--- AdventOfCodeTests/Day02Tests.cs
+++ AdventOfCodeTests/Day02Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day02Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "02";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("forward 5{0}down 5{0}forward 8{0}up 3{0}down 8{0}forward 2", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day02.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"150", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day02.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"1561344", result);
         }
 
         [TestMethod]
