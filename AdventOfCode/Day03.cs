97892eb9257cd9e9087447038211ed03c0ff0b7b
jlintvedt <jlintvedt@gmail.com>
2021-12-03 06:37:47 +0100

    Solve Day03 Puzzle1

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AdventOfCode
 {
@@ -7,15 +9,72 @@ namespace AdventOfCode
     /// </summary>
     public class Day03
     {
+        public class BinaryDiagnostics
+        {
+            private int[,] DiagnosticReport;
+            private int NumLines;
+            private int NumBits;
+
+            public BinaryDiagnostics(string input)
+            {
+                var lines = input.Split(Environment.NewLine);
+                NumLines = lines.Length;
+                NumBits = lines[0].Length;
+                DiagnosticReport = new int[NumLines,NumBits];
+
+                for (int i = 0; i < NumLines; i++)
+                {
+                    for (int j = 0; j < NumBits; j++)
+                    {
+                        if (lines[i][j] == '1')
+                        {
+                            DiagnosticReport[i, j] = 1;
+                        }
+                    }
+                }
+            }
+
+            public int CalculatePowerConsumption()
+            {
+                var numSet = new int[NumBits];
+
+                // Count bits set
+                for (int i = 0; i < NumLines; i++)
+                {
+                    for (int j = 0; j < NumBits; j++)
+                    {
+                        if (DiagnosticReport[i,j] == 1)
+                            numSet[j]++;
+                    }
+                }
+
+                UInt16 gamma = 0;
+                UInt16 epsilon = 0;
+                for (int i = 0; i < NumBits; i++)
+                {
+                    if (numSet[NumBits - 1 - i] > NumLines / 2)
+                        gamma |= (UInt16)(1 << i);
+                    else
+                        epsilon |= (UInt16)(1 << i);
+                }
+
+                return gamma * epsilon;
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var bd = new BinaryDiagnostics(input);
+
+            return bd.CalculatePowerConsumption().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
         public static string Puzzle2(string input)
         {
+            var bd = new BinaryDiagnostics(input);
+
             return "Puzzle2";
         }
     }
--- AdventOfCodeTests/Day03Tests.cs
+++ AdventOfCodeTests/Day03Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day03Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "03";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("00100{0}11110{0}10110{0}10111{0}10101{0}01111{0}00111{0}11100{0}10000{0}11001{0}00010{0}01010", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day03.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"198", result);
         }
 
         [TestMethod]
@@ -43,14 +42,14 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day03.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"1071734", result);
         }
 
         [TestMethod]
         public void Example_Puzzle2()
         {
             // Act
-            var result = AdventOfCode.Day03.Puzzle2(input_example2);
+            var result = AdventOfCode.Day03.Puzzle2(input_example1);
 
             // Assert
             Assert.AreEqual($"Puzzle2", result);
