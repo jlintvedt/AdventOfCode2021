928dcda49d6d079235f30f4e333273f192a3b09b
jlintvedt <jlintvedt@gmail.com>
2021-12-04 07:03:21 +0100

    Solve Day04 Puzzle1

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace AdventOfCode
 {
@@ -7,10 +10,120 @@ namespace AdventOfCode
     /// </summary>
     public class Day04
     {
+        public class BingoSubsystem
+        {
+            private Queue<int> NumberDraws = new Queue<int>();
+            private List<BingoBoard> BingoBoards = new List<BingoBoard>();
+
+            public BingoSubsystem(string input)
+            {
+                var sections = input.Split(new string[] { $"{Environment.NewLine}{Environment.NewLine}" }, StringSplitOptions.None);
+
+                // Parse numbers
+                foreach (var num in sections[0].Split(','))
+                {
+                    NumberDraws.Enqueue(int.Parse(num));
+                }
+
+                // Parse boards
+                for (int i = 1; i < sections.Length; i++)
+                {
+                    BingoBoards.Add(new BingoBoard(sections[i]));
+                }
+            }
+
+            public int FindWinningNumber()
+            {
+                while (NumberDraws.Count > 0)
+                {
+                    var number = NumberDraws.Dequeue();
+
+                    foreach (var board in BingoBoards)
+                    {
+                        if (board.MarkNumber(number))
+                        {
+                            return number * board.FindSumOfUnmarkedNumbers();
+                        }
+                    }
+                }
+
+                throw new Exception("Couldn't find any winning boards");
+            }
+
+            public class BingoBoard
+            {
+                private Dictionary<int, (int y, int x)> Numbers;
+                private bool[,] Marked;
+
+                public BingoBoard(string input)
+                {
+                    Numbers = new Dictionary<int, (int, int)>();
+                    Marked = new bool[5,5];
+
+                    var lines = input.Split(Environment.NewLine);
+                    for (int i = 0; i < 5; i++)
+                    {
+                        var numbers = lines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                        for (int j = 0; j < 5; j++)
+                        {
+                            Numbers.Add(int.Parse(numbers[j]), (i, j));
+                        }
+                    }
+                }
+
+                public bool MarkNumber(int number)
+                {
+                    if (!Numbers.TryGetValue(number, out (int y, int x) pos))
+                        return false;
+
+                    Marked[pos.y, pos.x] = true;
+
+                    // Check for complete column/row
+                    var isWinner = true;
+                    for (int x = 0; x < 5; x++)
+                    {
+                        if (!Marked[pos.y, x])
+                        {
+                            isWinner = false;
+                            break;
+                        }
+                    }
+
+                    if (isWinner)
+                        return true;
+
+                    for (int y = 0; y < 5; y++)
+                    {
+                        if (!Marked[y, pos.x])
+                            return false;
+                    }
+
+                    return true;
+                }
+
+                public int FindSumOfUnmarkedNumbers()
+                {
+                    int sum = 0;
+
+                    foreach (var (num, (y, x)) in Numbers)
+                    {
+                        if (!Marked[y, x])
+                        {
+                            sum += num;
+                        }
+                    }
+
+                    return sum;
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var bs = new BingoSubsystem(input);
+
+            return bs.FindWinningNumber().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day04Tests.cs
+++ AdventOfCodeTests/Day04Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day04Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "04";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1{0}{0}22 13 17 11  0{0} 8  2 23  4 24{0}21  9 14 16  7{0} 6 10  3 18  5{0} 1 12 20 15 19{0}{0} 3 15  0  2 22{0} 9 18 13 17  5{0}19  8  7 25 23{0}20 11 10 24  4{0}14 21 16 12  6{0}{0}14 21 17 24  4{0}10 16 15  9 19{0}18  8 23 26 20{0}22 11 13  6  5{0} 2  0 12  3  7", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day04.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"4512", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day04.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"2745", result);
         }
 
         [TestMethod]
