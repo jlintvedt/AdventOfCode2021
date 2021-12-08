bfcc105bd03e504ba0a44fe9b3142e46364f20c5
jlintvedt <jlintvedt@gmail.com>
2021-12-08 06:34:02 +0100

    Solve Day08 Puzzle1

--- AdventOfCode/Day08.cs
+++ AdventOfCode/Day08.cs
@@ -1,16 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
     /// <summary>
-    /// https://adventofcode.com/2021/day/08
+    /// https://adventofcode.com/2021/day/8
     /// </summary>
     public class Day08
     {
+        public class AA
+        {
+            private List<(List<Digit> patterns, List<Digit> output)> entries;
+
+            public AA(string input)
+            {
+                entries = new List<(List<Digit> patterns,List<Digit> output)> ();
+                var lines = input.Split(Environment.NewLine);
+
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(" | ");
+
+                    var patterns = new List<Digit>();
+                    foreach (var raw in parts[0].Split(' '))
+                    {
+                        patterns.Add(new Digit(raw));
+                    }
+
+                    var output = new List<Digit>();
+                    foreach (var raw in parts[1].Split(' '))
+                    {
+                        output.Add(new Digit(raw));
+                    }
+
+                    entries.Add((patterns, output));
+                }
+            }
+
+            public int CountUniqueNumOutputDigits()
+            {
+                var numDigits = 0;
+
+                foreach (var entry in entries)
+                {
+                    foreach (var digit in entry.output)
+                    {
+                        if (digit.NumSegments == 2 || digit.NumSegments == 4 || digit.NumSegments == 3 || digit.NumSegments == 7)
+                        {
+                            numDigits++;
+                        }
+                    }
+                }
+
+                return numDigits;
+            }
+
+            public class Digit
+            {
+                public int NumSegments;
+                public bool[] Segments = new bool[7];
+
+                public Digit(string input)
+                {
+                    foreach (var seg in input)
+                    {
+                        switch (seg)
+                        {
+                            case 'a': Segments[(int)Segment.a] = true; break;
+                            case 'b': Segments[(int)Segment.b] = true; break;
+                            case 'c': Segments[(int)Segment.c] = true; break;
+                            case 'd': Segments[(int)Segment.d] = true; break;
+                            case 'e': Segments[(int)Segment.e] = true; break;
+                            case 'f': Segments[(int)Segment.f] = true; break;
+                            case 'g': Segments[(int)Segment.g] = true; break;
+                            default: throw new ArgumentException($"Unknown segment {seg} in {input}");
+                        }
+
+                        NumSegments++;
+                    }
+                }
+            }
+
+            public enum Segment {
+                a,
+                b,
+                c,
+                d,
+                e,
+                f,
+                g,
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var aa = new AA(input);
+
+            return aa.CountUniqueNumOutputDigits().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day08Tests.cs
+++ AdventOfCodeTests/Day08Tests.cs
@@ -3,21 +3,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day08Tests
     {
         private string input_puzzle;
         private string input_example1;
-        private string input_example2;
 
         [TestInitialize]
         public void LoadInput()
         {
             string day = "08";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
-            input_example2 = string.Format("example{0}2", Environment.NewLine);
+            input_example1 = string.Format("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe{0}edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc{0}fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg{0}fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb{0}aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea{0}fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb{0}dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe{0}bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef{0}egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb{0}gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce", Environment.NewLine);
         }
 
         [TestMethod]
@@ -33,7 +30,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day08.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"26", result);
         }
 
         [TestMethod]
@@ -43,14 +40,14 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day08.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"530", result);
         }
 
         [TestMethod]
         public void Example_Puzzle2()
         {
             // Act
-            var result = AdventOfCode.Day08.Puzzle2(input_example2);
+            var result = AdventOfCode.Day08.Puzzle2(input_example1);
 
             // Assert
             Assert.AreEqual($"Puzzle2", result);
