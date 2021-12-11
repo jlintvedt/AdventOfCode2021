2370e526cbce9526ece7909ac57981872604d5df
jlintvedt <jlintvedt@gmail.com>
2021-12-11 06:47:02 +0100

    Solve Day11 Puzzle1

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode
 {
@@ -7,10 +9,94 @@ namespace AdventOfCode
     /// </summary>
     public class Day11
     {
+        public class DumboOctopus
+        {
+            private int[,] energyLevel;
+            private int height = 10;
+            private int width = 10;
+            private readonly HashSet<(int y, int x)> isFlashing;
+            private readonly HashSet<(int y, int x)> hasFlashed;
+            private static readonly List<(int y, int x)> neighbourOffset = new List<(int y, int x)>(){
+                (-1, -1), (-1, 0), (-1, 1), // Above
+                (0, -1), (0, 1),            // Sides
+                (1, -1), (1, 0), (1, 1)     // Below
+            };
+
+            public DumboOctopus (string input)
+            {
+                energyLevel = new int[10, 10];
+                isFlashing = new HashSet<(int y, int x)>();
+                hasFlashed = new HashSet<(int y, int x)>();
+
+                var lines = input.Split(Environment.NewLine);
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        energyLevel[y,x] = (int)char.GetNumericValue(lines[y][x]);
+                    }
+                }
+            }
+
+            public int FindNumFlashes(int steps = 100)
+            {
+                var numFlashes = 0;
+
+                for (int step = 0; step < steps; step++)
+                {
+                    // Increase all by one
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            if (++energyLevel[y, x] > 9)
+                                isFlashing.Add((y, x));
+                        }
+                    }
+
+                    // Flash all with maximum energy
+                    while (isFlashing.Count > 0)
+                    {
+                        var octo = isFlashing.First();
+                        FlashOctopus(octo);
+                        isFlashing.Remove(octo);
+                        hasFlashed.Add(octo);
+                        numFlashes++;
+                    }
+
+                    // Reset energy of all flashed octopus
+                    foreach (var (y, x) in hasFlashed)
+                    {
+                        energyLevel[y, x] = 0;
+                    }
+                    hasFlashed.Clear();
+                }
+
+                return numFlashes;
+            }
+
+            private void FlashOctopus((int y, int x) octo)
+            {
+                foreach (var offset in neighbourOffset)
+                {
+                    var neighbour = (y: octo.y + offset.y, x: octo.x + offset.x);
+
+                    // Check if widhin grid
+                    if (0 > neighbour.y || neighbour.y >= height || 0 > neighbour.x || neighbour.x >= width)
+                        continue;
+
+                    if (++energyLevel[neighbour.y, neighbour.x] == 10)
+                        isFlashing.Add(neighbour);
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var doc = new DumboOctopus(input);
+
+            return doc.FindNumFlashes().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day11Tests.cs
+++ AdventOfCodeTests/Day11Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day11Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "11";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("5483143223{0}2745854711{0}5264556173{0}6141336146{0}6357385478{0}4167524645{0}2176841721{0}6882881134{0}4846848554{0}5283751526", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day11.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"1656", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day11.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"1757", result);
         }
 
         [TestMethod]
