7c251b99b7fbef9074f78c41477b8480ed9500e5
jlintvedt <jlintvedt@gmail.com>
2021-12-19 10:11:02 +0100

    Solve Day18 Puzzle1

--- AdventOfCode/Day18.cs
+++ AdventOfCode/Day18.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,246 @@ namespace AdventOfCode
     /// </summary>
     public class Day18
     {
+        public class SnailfishMath
+        {
+            private readonly Queue<Number> numbers = new();
+            private Number sum;
+
+            public SnailfishMath(string input)
+            {
+                foreach (var line in input.Split(Environment.NewLine))
+                    numbers.Enqueue(new Number(line));
+            }
+
+            public Number Sum { 
+                get {
+                    if (sum == null)
+                        CalculateSum();
+                    return sum; 
+            } }
+
+            public int FindMagnitudeOfFinalSum()
+            {
+                CalculateSum();
+                return sum.CalculateMagnitude();
+            }
+
+            private void CalculateSum()
+            {
+                sum = numbers.Dequeue();
+                sum.Reduce();
+                while (numbers.Count > 0)
+                {
+                    sum.AddNumber(numbers.Dequeue());
+                    sum.Reduce();
+                }
+            }
+
+            public class Number
+            {
+                public readonly List<Element> Elements;
+
+                public Number(string input)
+                {
+                    Elements = new List<Element>();
+                    foreach (var e in input)
+                        Elements.Add(new Element(e));
+                }
+
+                public void AddNumber(Number other)
+                {
+                    Elements.Insert(0, new Element('['));
+                    Elements.Insert(Elements.Count, new Element(','));
+                    Elements.InsertRange(Elements.Count, other.Elements);
+                    Elements.Insert(Elements.Count, new Element(']'));
+                }
+
+                public void Reduce()
+                {
+                    while (true)
+                    {
+                        // Search for explode or split candidate
+                        int depth = 0;
+                        int explodeIndex = 0, splitIndex = 0;
+                        for (int i = 0; i < Elements.Count; i++)
+                        {
+                            switch (Elements[i].Type)
+                            {
+                                case Type.value:
+                                    // Set to index of first split-ready number
+                                    if (splitIndex == 0 && Elements[i].Value >= 10)
+                                        splitIndex = i;
+                                    break;
+                                case Type.open:
+                                    if (++depth > 4)
+                                        explodeIndex = i;
+                                    break;
+                                case Type.close:
+                                    depth--;
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                            if (explodeIndex > 0)
+                                break;
+                        }
+
+                        // Perform explosion
+                        if (explodeIndex > 0)
+                        {
+                            PerformExplosion(explodeIndex);
+                            continue;
+                        }
+
+                        // Perform split
+                        if (splitIndex > 0)
+                        {
+                            PerformSplit(splitIndex);
+                            continue;
+                        }
+
+                        // No more operations
+                        break;
+                    }
+                }
+
+                public void PerformExplosion(int index)
+                {
+                    if (Elements[index].Type != Type.open || Elements[index + 1].Type != Type.value || Elements[index + 2].Type != Type.separator || Elements[index + 3].Type != Type.value || Elements[index + 4].Type != Type.close)
+                        throw new Exception("Invalid index for explosion");
+
+                    // Move left value
+                    for (int i = index; i > 0; i--)
+                    {
+                        if (Elements[i].Type == Type.value)
+                        {
+                            Elements[i].Value += Elements[index + 1].Value;
+                            break;
+                        }
+                    }
+
+                    // Move right value
+                    for (int i = index+5; i < Elements.Count; i++)
+                    {
+                        if (Elements[i].Type == Type.value)
+                        {
+                            Elements[i].Value += Elements[index + 3].Value;
+                            break;
+                        }
+                    }
+
+                    // Replace pair with 0
+                    Elements.RemoveRange(index, 5);
+                    Elements.Insert(index, new Element(0));
+                }
+
+                public void PerformSplit(int index)
+                {
+                    if (Elements[index].Type != Type.value && Elements[index].Value < 10)
+                        throw new Exception("Invalid index for split");
+
+                    var half = Elements[index].Value / 2;
+                    var remainder = Elements[index].Value % 2;
+
+                    Elements.Insert(index + 1, new Element(']'));
+                    Elements.Insert(index + 1, new Element(half+remainder));
+                    Elements.Insert(index + 1, new Element(','));
+                    Elements[index].Value = half;
+                    Elements.Insert(index, new Element('['));
+                }
+
+                public int CalculateMagnitude()
+                {
+                    var index = 0;
+                    while (Elements.Count > 1)
+                    {
+                        // Search for closing bracket
+                        while (Elements[++index].Type != Type.close) { }
+                        // Move index to start of inner pair
+                        index -= 4;
+                        // Collapse
+                        PerformMagnitudeCollapse(index);
+                    }
+
+                    return Elements[0].Value;
+                }
+
+                public void PerformMagnitudeCollapse(int index)
+                {
+                    if (Elements[index].Type != Type.open || Elements[index + 1].Type != Type.value || Elements[index + 2].Type != Type.separator || Elements[index + 3].Type != Type.value || Elements[index + 4].Type != Type.close)
+                        throw new Exception("Invalid index for magnitude collapse");
+
+                    var elem = new Element(Elements[index + 1].Value * 3 + Elements[index + 3].Value * 2);
+                    Elements.RemoveRange(index, 5);
+                    Elements.Insert(index, elem);
+                }
+
+                public override string ToString()
+                {
+                    return String.Join("", Elements);
+                }
+
+                public class Element
+                {
+                    public Type Type;
+                    public int Value;
+
+                    public Element(char input)
+                    {
+                        switch (input)
+                        {
+                            case '[':
+                                Type = Type.open;
+                                break;
+                            case ']':
+                                Type = Type.close;
+                                break;
+                            case ',':
+                                Type = Type.separator;
+                                break;
+                            default:
+                                Type = Type.value;
+                                Value = (int)char.GetNumericValue(input);
+                                break;
+                        }
+                    }
+
+                    public Element(int value)
+                    {
+                        Type = Type.value;
+                        Value = value;
+                    }
+
+                    public override string ToString()
+                    {
+                        return Type switch
+                        {
+                            Type.value => Value.ToString(),
+                            Type.open => "[",
+                            Type.close => "]",
+                            Type.separator => ",",
+                            _ => throw new Exception("Unknown type"),
+                        };
+                    }
+                }
+            }
+
+            public enum Type
+            {
+                value,
+                open,
+                close,
+                separator
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var sm = new SnailfishMath(input);
+
+            return sm.FindMagnitudeOfFinalSum().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day18Tests.cs
+++ AdventOfCodeTests/Day18Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day18Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "18";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]{0}[[[5,[2,8]],4],[5,[[9,9],0]]]{0}[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]{0}[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]{0}[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]{0}[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]{0}[[[[5,4],[7,7]],8],[[8,3],8]]{0}[[9,3],[[9,9],[6,[4,9]]]]{0}[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]{0}[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day18.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"4140", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day18.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"4057", result);
         }
 
         [TestMethod]
@@ -64,6 +63,149 @@ namespace AdventOfCodeTests
 
             // Assert
             Assert.AreEqual($"Puzzle2", result);
+        }
+
+        // Number
+        [TestMethod]
+        public void Number_IsParsedCorrectly()
+        {
+            // Arrange, Act & Assert
+            var input = "[1,2]";
+            Assert.AreEqual(input, (new AdventOfCode.Day18.SnailfishMath.Number(input)).ToString());
+
+            input = "[[1,2],3]";
+            Assert.AreEqual(input, (new AdventOfCode.Day18.SnailfishMath.Number(input)).ToString());
+
+            input = "[9,[8,7]]";
+            Assert.AreEqual(input, (new AdventOfCode.Day18.SnailfishMath.Number(input)).ToString());
+
+            input = "[[1,9],[8,5]]";
+            Assert.AreEqual(input, (new AdventOfCode.Day18.SnailfishMath.Number(input)).ToString());
+
+            input = "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]";
+            Assert.AreEqual(input, (new AdventOfCode.Day18.SnailfishMath.Number(input)).ToString());
+
+            input = "[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]";
+            Assert.AreEqual(input, (new AdventOfCode.Day18.SnailfishMath.Number(input)).ToString());
+
+            input = "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]";
+            Assert.AreEqual(input, (new AdventOfCode.Day18.SnailfishMath.Number(input)).ToString());
+        }
+
+        [TestMethod]
+        public void Number_PerformExplosion()
+        {
+            // Arrange, Act & Assert
+            var number = new AdventOfCode.Day18.SnailfishMath.Number("[[[[[9,8],1],2],3],4]");
+            number.PerformExplosion(4);
+            Assert.AreEqual("[[[[0,9],2],3],4]", number.ToString());
+
+            number = new AdventOfCode.Day18.SnailfishMath.Number("[7,[6,[5,[4,[3,2]]]]]");
+            number.PerformExplosion(12);
+            Assert.AreEqual("[7,[6,[5,[7,0]]]]", number.ToString());
+
+            number = new AdventOfCode.Day18.SnailfishMath.Number("[[6,[5,[4,[3,2]]]],1]");
+            number.PerformExplosion(10);
+            Assert.AreEqual("[[6,[5,[7,0]]],3]", number.ToString());
+
+            number = new AdventOfCode.Day18.SnailfishMath.Number("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]");
+            number.PerformExplosion(10);
+            Assert.AreEqual("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", number.ToString());
+
+            number = new AdventOfCode.Day18.SnailfishMath.Number("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]");
+            number.PerformExplosion(24);
+            Assert.AreEqual("[[3,[2,[8,0]]],[9,[5,[7,0]]]]", number.ToString());
+        }
+
+        [TestMethod]
+        public void Number_ReduceStepByStep()
+        {
+            // Arrange
+            var number = new AdventOfCode.Day18.SnailfishMath.Number("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]");
+
+            // Act & Assert
+            number.PerformExplosion(4);
+            Assert.AreEqual("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", number.ToString());
+
+            number.PerformExplosion(16);
+            Assert.AreEqual("[[[[0,7],4],[15,[0,13]]],[1,1]]", number.ToString());
+
+            number.PerformSplit(13);
+            Assert.AreEqual("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", number.ToString());
+
+            number.PerformSplit(22);
+            Assert.AreEqual("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]", number.ToString());
+
+            number.PerformExplosion(22);
+            Assert.AreEqual("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", number.ToString());
+        }
+
+        [TestMethod]
+        public void Number_AddNumber()
+        {
+            // Arrange, Act & Assert
+            var number = new AdventOfCode.Day18.SnailfishMath.Number("[1,1]");
+            var other= new AdventOfCode.Day18.SnailfishMath.Number("[2,2]");
+            number.AddNumber(other);
+            Assert.AreEqual("[[1,1],[2,2]]", number.ToString());
+
+            number = new AdventOfCode.Day18.SnailfishMath.Number("[[[[4,3],4],4],[7,[[8,4],9]]]");
+            other = new AdventOfCode.Day18.SnailfishMath.Number("[1,1]");
+            number.AddNumber(other);
+            Assert.AreEqual("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", number.ToString());
+        }
+
+        [TestMethod]
+        public void Pilotfish_Sum()
+        {
+            // Arrange, Act & Assert
+            var input = string.Format("[1,1]{0}[2,2]{0}[3,3]{0}[4,4]", Environment.NewLine);
+            var sm = new AdventOfCode.Day18.SnailfishMath(input);
+            Assert.AreEqual("[[[[1,1],[2,2]],[3,3]],[4,4]]", sm.Sum.ToString());
+
+            input = string.Format("[1,1]{0}[2,2]{0}[3,3]{0}[4,4]{0}[5,5]", Environment.NewLine);
+            sm = new AdventOfCode.Day18.SnailfishMath(input);
+            Assert.AreEqual("[[[[3,0],[5,3]],[4,4]],[5,5]]", sm.Sum.ToString());
+
+            input = string.Format("[1,1]{0}[2,2]{0}[3,3]{0}[4,4]{0}[5,5]{0}[6,6]", Environment.NewLine);
+            sm = new AdventOfCode.Day18.SnailfishMath(input);
+            Assert.AreEqual("[[[[5,0],[7,4]],[5,5]],[6,6]]", sm.Sum.ToString());
+
+            input = string.Format("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]{0}[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]{0}[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]{0}[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]{0}[7,[5,[[3,8],[1,4]]]]{0}[[2,[2,2]],[8,[8,1]]]{0}[2,9]{0}[1,[[[9,3],9],[[9,0],[0,7]]]]{0}[[[5,[7,4]],7],1]{0}[[[[4,2],2],6],[8,7]]", Environment.NewLine);
+            sm = new AdventOfCode.Day18.SnailfishMath(input);
+            Assert.AreEqual("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", sm.Sum.ToString());
+        }
+
+        [TestMethod]
+        public void Pilotfish_FindMagnitudeOfFinalSum()
+        {
+            // Arrange, Act & Assert
+            var sm = new AdventOfCode.Day18.SnailfishMath("[9,1]");
+            Assert.AreEqual(29, sm.FindMagnitudeOfFinalSum());
+
+            sm = new AdventOfCode.Day18.SnailfishMath("[1,9]");
+            Assert.AreEqual(21, sm.FindMagnitudeOfFinalSum());
+
+            sm = new AdventOfCode.Day18.SnailfishMath("[[9,1],[1,9]]");
+            Assert.AreEqual(129, sm.FindMagnitudeOfFinalSum());
+
+            sm = new AdventOfCode.Day18.SnailfishMath("[[1,2],[[3,4],5]]");
+            Assert.AreEqual(143, sm.FindMagnitudeOfFinalSum());
+
+            sm = new AdventOfCode.Day18.SnailfishMath("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]");
+            Assert.AreEqual(1384, sm.FindMagnitudeOfFinalSum());
+
+            sm = new AdventOfCode.Day18.SnailfishMath("[[[[1,1],[2,2]],[3,3]],[4,4]]");
+            Assert.AreEqual(445, sm.FindMagnitudeOfFinalSum());
+
+            sm = new AdventOfCode.Day18.SnailfishMath("[[[[3,0],[5,3]],[4,4]],[5,5]]");
+            Assert.AreEqual(791, sm.FindMagnitudeOfFinalSum());
+
+            sm = new AdventOfCode.Day18.SnailfishMath("[[[[5,0],[7,4]],[5,5]],[6,6]]");
+            Assert.AreEqual(1137, sm.FindMagnitudeOfFinalSum());
+
+            sm = new AdventOfCode.Day18.SnailfishMath("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
+            Assert.AreEqual(3488, sm.FindMagnitudeOfFinalSum());
         }
     }
 }
